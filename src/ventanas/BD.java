f5136fdeee2a2b8a3ecd7ff8ea03f00450dc7cd1
Jon <Jon@Asus>
2019-11-25 18:04:11 +0100

    bd connection

---
+++ src/ventanas/BD.java
@@ -0,0 +1,45 @@
+package ventanas;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class BD {
+	public static Connection initBD( String nombreBD ) {
+		try {
+		    Class.forName("org.sqlite.JDBC");
+		    Connection con = DriverManager.getConnection("jdbc:sqlite:" + nombreBD );
+			log( Level.INFO, "Conectada base de datos " + nombreBD, null );
+		    return con;
+		} catch (ClassNotFoundException | SQLException e) {
+			log( Level.SEVERE, "Error en conexión de base de datos " + nombreBD, e );
+			return null;
+		}
+	}
+	private static Logger logger = null;
+	
+	// Metodo local para loggear
+	private static void log( Level level, String msg, Throwable excepcion ) {
+		if (logger==null) {  // Logger por defecto local:
+			logger = Logger.getLogger( BD.class.getName() );  // Nombre del logger - el de la clase
+			logger.setLevel( Level.ALL );  // Loguea todos los niveles
+		}
+		if (excepcion==null)
+			logger.log( level, msg );
+		else
+			logger.log( level, msg, excepcion );
+	}
+	public static void cerrarBD( Connection con, Statement st ) {
+		try {
+			if (st!=null) st.close();
+			if (con!=null) con.close();
+			log( Level.INFO, "Cierre de base de datos", null );
+		} catch (SQLException e) {
+			log( Level.SEVERE, "Error en cierre de base de datos", e );
+		}
+	}
+}
