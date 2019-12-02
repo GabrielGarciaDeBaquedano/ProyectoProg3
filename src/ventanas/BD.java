929cccf830eb2e335c16ef29edea08fe200f3a50
Jon Churruca Zabala <Jon Churruca Zabala@10.164.61.135>
2019-12-02 20:38:42 +0100

    Toques en la base de datos y las clases de jugador, partida y juego

--- src/ventanas/BD.java
+++ src/ventanas/BD.java
@@ -2,44 +2,76 @@ package ventanas;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.swing.JOptionPane;
+
 
 public class BD {
-	public static Connection initBD( String nombreBD ) {
-		try {
-		    Class.forName("org.sqlite.JDBC");
-		    Connection con = DriverManager.getConnection("jdbc:sqlite:" + nombreBD );
-			log( Level.INFO, "Conectada base de datos " + nombreBD, null );
-		    return con;
-		} catch (ClassNotFoundException | SQLException e) {
-			log( Level.SEVERE, "Error en conexión de base de datos " + nombreBD, e );
-			return null;
-		}
-	}
+	
+	protected static Connection con;	
 	private static Logger logger = null;
 	
-	// Metodo local para loggear
-	private static void log( Level level, String msg, Throwable excepcion ) {
-		if (logger==null) {  // Logger por defecto local:
-			logger = Logger.getLogger( BD.class.getName() );  // Nombre del logger - el de la clase
-			logger.setLevel( Level.ALL );  // Loguea todos los niveles
+	public static boolean initBD( String nombreBD ) {
+		try {
+		    Class.forName("org.sqlite.JDBC");
+		    con = DriverManager.getConnection("jdbc:sqlite:" + nombreBD );
+			log( Level.INFO, "Conectada base de datos " + nombreBD, null );
+			Statement st = con.createStatement();
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Jugador(idJugador INTEGER PRIMARY KEY AUTOINCREMENT,  nombreJugador VARCHAR(100);");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Partida(codPartida INTEGER PRIMARY KEY AUTOINCREMENT,  puntuacion INTEGER, tiempoPartida DECIMAL(5, 2), fechaPartida DATE ;");
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS Juego(idJuego INTEGER PRIMARY KEY AUTOINCREMENT,  nombreJuego VARCHAR(100);");
+			
+		    return true;
+		} catch (ClassNotFoundException | SQLException e) {
+			log( Level.SEVERE, "Error en conexiï¿½n de base de datos " + nombreBD, e );
+			return false;
 		}
-		if (excepcion==null)
-			logger.log( level, msg );
-		else
-			logger.log( level, msg, excepcion );
 	}
-	public static void cerrarBD( Connection con, Statement st ) {
+	
+
+	private static void insertarJugador( String nombreJugador) {
+		try {
+			Statement st = con.createStatement();
+			try {
+				String sent = "insert into Jugador(idJugador, nombreJugador) values(1," + nombreJugador +" );";
+				
+				
+			}catch(Exception e) {
+				}
+		
+		}catch( Exception e ) {
+						e.printStackTrace();
+						JOptionPane.showInputDialog("No se ha podido recorrer la lista de jugadores!",  JOptionPane.WARNING_MESSAGE);
+		}
+	}
+	
+
+	public static void cerrarBD( Statement st ) {
 		try {
 			if (st!=null) st.close();
-			if (con!=null) con.close();
+			if (con != null) con.close();
 			log( Level.INFO, "Cierre de base de datos", null );
 		} catch (SQLException e) {
 			log( Level.SEVERE, "Error en cierre de base de datos", e );
 		}
 	}
+	
+	// Metodo local para loggear
+		private static void log( Level level, String msg, Throwable excepcion ) {
+			if (logger==null) {  // Logger por defecto local:
+				logger = Logger.getLogger( BD.class.getName() );  // Nombre del logger - el de la clase
+				logger.setLevel( Level.ALL );  // Loguea todos los niveles
+			}
+			if (excepcion==null)
+				logger.log( level, msg );
+			else
+				logger.log( level, msg, excepcion );
+		}
 }
